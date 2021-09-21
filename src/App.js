a0f726cd079e455964a13f266dfb52acac4bac0d
nick.molloy <nick@sproulmolloy.com>
2021-09-21 14:12:25 -0400

    add authenticator

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import DisplayPosts from './components/DisplayPosts'
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
 
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
       <CreatePost />
       <DisplayPosts />
-
+    
 
     </div>
+    </AmplifyAuthenticator>
   );
 }
 
