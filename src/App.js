798e2635e4c8c94f0ebc1059a64b2616a7499b9e
nick.molloy <nick@sproulmolloy.com>
2021-09-21 20:20:06 -0400

    back to original document

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 import DisplayPosts from './components/DisplayPosts'
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
-import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
 
 
 function App() {
   return (
-    <AmplifyAuthenticator>
     <div className="App">
       <CreatePost />
       <DisplayPosts />
-    
-
     </div>
-    </AmplifyAuthenticator>
   );
 }
 
